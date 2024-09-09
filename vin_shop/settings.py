21a90cc4881920bd328c07e8c752f6b7b176791b
Kevin Adriano <kevin.adriano@cs.ui.ac.id>
2024-09-09 15:11:59 +0700

    added all

--- vin_shop/settings.py
+++ vin_shop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!$y@5lip%p$v(x7))bxdg^*a$$@2ops9xzd$++e2wi=otz%dml
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","kevin-adriano-vinshop.pbp.cs.ui.ac.id"]
 
 
 # Application definition
