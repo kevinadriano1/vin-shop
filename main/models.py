42defc67ea6872b8fe2b53d8bb003f0e95f59239
Kevin Adriano <kevin.adriano@cs.ui.ac.id>
2024-09-20 21:00:39 +0700

    assignment4

--- main/models.py
+++ main/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 import uuid
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     description = models.TextField()
